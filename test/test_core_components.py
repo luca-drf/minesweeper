033ea2ce6755de432541629f0e83f0be8366c0db
Luca Da Rin Fioretto <radome@gmail.com>
2020-04-09 21:04:55 +0100

    Add tests

--- test/test_core_components.py
+++ test/test_core_components.py
@@ -12,3 +12,73 @@ def test_cell_defaults():
     assert cell.col == 2
 
 
+def test_cell_clear_default():
+    cell = Cell(1, 2)
+    cell.clear()
+    assert cell.cleared
+
+
+def test_cell_clear_cleared():
+    cell = Cell(1, 2, cleared=True)
+    with pytest.raises(RuntimeError):
+        cell.clear()
+    assert cell.cleared
+
+
+def test_cell_clear_flagged():
+    cell = Cell(1, 2, flagged=True)
+    with pytest.raises(RuntimeError):
+        cell.clear()
+    assert not cell.cleared
+
+
+def test_cell_counter_default():
+    cell = Cell(1, 2)
+    assert cell.counter == 0
+
+
+def test_cell_counter_error():
+    cell = Cell(1, 2)
+    with pytest.raises(ValueError):
+        cell.counter = 9
+    with pytest.raises(ValueError):
+        cell.counter = -1
+
+
+def test_cell_flag_default():
+    cell = Cell(1, 2)
+    assert not cell.flagged
+    cell.flag()
+    assert cell.flagged
+
+
+def test_cell_flag_error():
+    cell = Cell(1, 2, cleared=True)
+    with pytest.raises(RuntimeError):
+        cell.flag()
+
+
+def test_cell_unflag():
+    cell = Cell(1, 2, flagged=True)
+    assert cell.flagged
+    cell.unflag()
+    assert not cell.flagged
+
+
+def test_cell_unflag_error():
+    cell = Cell(1, 2)
+    with pytest.raises(RuntimeError):
+        cell.unflag()
+
+
+@pytest.fixture
+def default_grid():
+    return Grid(6, 7)
+
+
+def test_grid_cell_coord(default_grid):
+    pass
+
+
+def test_grid_cell_place_bombs(default_grid):
+    pass
